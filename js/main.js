fd74d5c5da9be0e63af9faf91712b74243dbd22b
Racebig <racebig9@gmail.com>
2020-12-03 20:48:58 +0100

    Adding login form html/css/javascript

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+//DOM Manipulation
+
+const DOM = (() => {
+
+    const registerForm = document.getElementById("register-form");
+    const loginForm = document.getElementById("login-form");
+
+    const displayRegisterForm = () => {
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+    }
+
+    const displayLoginForm = () => {
+        registerForm.style.display = "none";
+        loginForm.style.display = "block";
+        loginForm.style.marginTop = "250px";
+    }
+
+    const closeForms = () => {
+        registerForm.style.display = "none";
+        loginForm.style.display = "none";
+    }
+
+
+    return {
+        displayRegisterForm,
+        displayLoginForm,
+        closeForms
+    }
+
+})();
